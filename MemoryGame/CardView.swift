10857e33956d472d6fc2f34bc68bae21411a2691
Pawel Ciszewski <kardahim@gmail.com>
2024-04-27 10:15:51 +0200

    zad1

---
+++ MemoryGame/CardView.swift
@@ -0,0 +1,41 @@
+//
+//  CardView.swift
+//  MemoryGame
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var content: String//  = "😄"
+    @State var isFaceUp: Bool = true
+    
+    init(content: String) {
+        self.content = content
+    }
+    
+    var body: some View {
+        let rectangle = RoundedRectangle(cornerRadius: 28)
+        ZStack{
+            Group{
+                rectangle.fill(.white)
+                rectangle.strokeBorder(style: StrokeStyle(lineWidth: 2))
+                Text(content)
+                    .font(.largeTitle)
+            }
+            .opacity(isFaceUp ? 1 : 0)
+            
+            rectangle.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+          
+
+    }
+}
+
+//#Preview {
+//    CardView()
+//}
--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var contentArray = ["😄", "😄", "😂", "😂", "💀", "💀", "👍", "👍"]
+    @State var content = "😄"
+    @State var cardsCount = 4
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            cardDisplay
+            cardsNumberAdjuster
         }
         .padding()
+    }
+    
+    var cardDisplay: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0 ..< cardsCount, id: \.self) { i in
+                CardView(content: contentArray[i])
+            }
+        }
+        .foregroundColor(.blue)
+    }
+    
+    var cardsNumberAdjuster: some View {
+        HStack{
+            adjustCardNumber(by: -2, symbol: "minus.square")
+            adjustCardNumber(by: 2, symbol: "plus.square")
+        }
+    }
+    
+    func adjustCardNumber(by offset: Int, symbol: String) -> some View {
+        Button(action: {cardsCount += offset}, label: {Image(systemName: symbol)})
+            .disabled(cardsCount + offset < 2 || cardsCount + offset >= contentArray.count)
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(content: "😄")
 }
