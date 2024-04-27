d0e00fb267c9c7622175c5315d325149103d3089
Pawel Ciszewski <kardahim@gmail.com>
2024-04-27 08:09:12 +0200

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
